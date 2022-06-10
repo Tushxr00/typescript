746b88005797e020d96ae403709d09556ae1939b
Tushar <tushar.nandy@vidyamantra.com>
2022-06-10 17:56:09 +0530

    watching and automatically transpiling the ts file

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 var number = 12;
 console.log(number);
-var firstname = "mario";
+var firstname = "luige";
 console.log(firstname);
+var input = document.querySelectorAll("input");
+console.log(input);
+input.forEach(function (input) {
+    console.log(input);
+});
+/**
+ * * tsc app.ts
+ * "tsc app.ts" is complied or transpile only once and needs to be manually written in the terminal to transpile the file again
+ * * tsc app.ts -w
+ * by writting "tsc app.ts -w" we can watch a file and re complie or transpile it whenever there is any new changes to it
+ *
+ *
+ */ 
--- app.ts
+++ app.ts
@@ -2,6 +2,27 @@ const number = 12;
 
 console.log(number);
 
-const firstname = "mario";
+const firstname = "luige";
 
 console.log(firstname)
+
+const input = document.querySelectorAll("input")
+
+console.log(input)
+
+input.forEach(input=>{
+    console.log(input)
+})
+
+
+
+
+
+/**
+ * * tsc app.ts  
+ * "tsc app.ts" is complied or transpile only once and needs to be manually written in the terminal to transpile the file again
+ * * tsc app.ts -w 
+ * by writting "tsc app.ts -w" we can watch a file and re complie or transpile it whenever there is any new changes to it
+ * 
+ * 
+ */ 
